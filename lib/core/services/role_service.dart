24a971be93fb4545025fda57f8598bd4c42e3841
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2023-02-26 21:42:34 +0300

    new service methods added

--- lib/core/services/role_service.dart
+++ lib/core/services/role_service.dart
@@ -6,27 +6,38 @@ mixin RoleService {
   //Get
 
   Future<ResponseModel> getRoleById({required String id}) async {
-    ResponseModel response = await Executer.get(endpoint: "role/$id");
+    ResponseModel response = await Executer.get(
+        endpoint: BaseUrl.resource.getURl(
+      "role/$id",
+    ));
     return response;
   }
 
   Future<ResponseModel> getRole() async {
-    ResponseModel response = await Executer.get(endpoint: "role");
+    ResponseModel response = await Executer.get(
+        endpoint: BaseUrl.resource.getURl(
+      "role",
+    ));
     return response;
   }
 
   //Post
 
   Future<ResponseModel> postRole(RoleModel model) async {
-    ResponseModel response =
-        await Executer.post(endpoint: "role", data: model.toMap());
+    ResponseModel response = await Executer.post(
+        endpoint: BaseUrl.resource.getURl(
+      "role",
+    ));
     return response;
   }
 
   //Delete
 
   Future<ResponseModel> deleteRole(String id) async {
-    ResponseModel response = await Executer.delete(endpoint: "role/$id");
+    ResponseModel response = await Executer.delete(
+        endpoint: BaseUrl.resource.getURl(
+      "role/$id",
+    ));
     return response;
   }
 }
--- lib/core/services/tag_service.dart
+++ lib/core/services/tag_service.dart
@@ -4,23 +4,31 @@ mixin TagService {
 //Get
 
   Future<ResponseModel> getTag([int page = 0, int size = 100]) async {
-    ResponseModel response =
-        await Executer.get(endpoint: 'tag?page=$page&pageSize=$size');
+    ResponseModel response = await Executer.get(
+        endpoint: BaseUrl.resource.getURl(
+      "tag?page=$page&pageSize=$size",
+    ));
+
     return response;
   }
 
 //Post
 
   Future<ResponseModel> postTag(TagModel model) async {
-    ResponseModel response =
-        await Executer.post(endpoint: "tag", data: model.toMap());
+    ResponseModel response = await Executer.post(
+        endpoint: BaseUrl.resource.getURl(
+      "tag",
+    ));
     return response;
   }
 
 //Delete
 
   Future<ResponseModel> deleteTag(String tagName) async {
-    ResponseModel response = await Executer.delete(endpoint: "tag/$tagName");
+    ResponseModel response = await Executer.delete(
+        endpoint: BaseUrl.resource.getURl(
+      "tag/$tagName",
+    ));
     return response;
   }
 }
