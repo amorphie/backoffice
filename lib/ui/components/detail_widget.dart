3b0790aba95b2a5ad4285bac8d89799e3129d8c6
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2023-04-14 10:01:24 +0300

    workflow temp added

--- lib/ui/components/detail_widget.dart
+++ lib/ui/components/detail_widget.dart
@@ -17,17 +17,21 @@ class DetailWidget extends StatefulWidget {
   State<DetailWidget> createState() => _DetailWidgetState();
 }
 
-class _DetailWidgetState extends State<DetailWidget> with TickerProviderStateMixin {
+class _DetailWidgetState extends State<DetailWidget>
+    with TickerProviderStateMixin {
   late TabController _tabController;
-
   final AppMenuController menuController = Get.find<AppMenuController>();
   final EntityController entityController = Get.find<EntityController>();
   final HomeController homeController = Get.find<HomeController>();
+  List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+  late String dropdownValue;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: entityController.entity.display!.tabs!.length, vsync: this);
+    _tabController = TabController(
+        length: entityController.entity.display!.tabs!.length, vsync: this);
+    dropdownValue = list.first;
   }
 
   @override
@@ -36,78 +40,151 @@ class _DetailWidgetState extends State<DetailWidget> with TickerProviderStateMix
       padding: const EdgeInsets.all(8.0),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10.0),
-        child: Container(
-          width: MediaQuery.of(context).size.width / 2,
-          height: MediaQuery.of(context).size.height / 1.14,
-          child: Scaffold(
-              backgroundColor: KC.background,
-              appBar: AppBar(
-                toolbarHeight: 90,
-                backgroundColor: KC.primary,
-                elevation: 1,
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Icon(Icons.accessibility),
-                        SizedBox(width: 10),
-                        Text(
-                          "fullName",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 7),
-                    Row(
-                      children: [
-                        Text(
-                          "Tags : tags",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.white70, fontSize: 14, fontWeight: FontWeight.w400),
-                        ),
-                        SizedBox(width: 8),
-                        GestureDetector(
-                          onTap: () {
-                            // tagPopUp(context);
-                          },
-                          child: Icon(
-                            Icons.edit,
-                            size: 18,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                actions: [
-                  IconButton(
-                      onPressed: () {
-                        homeController.reset();
-                      },
-                      icon: Icon(Icons.close))
-                ],
-                bottom: TabBar(
-                    controller: _tabController,
-                    tabs: entityController.entity.display!.tabs!
-                        .map(
-                          (e) => Tab(
-                            icon: Text(e.title.trTR),
-                          ),
-                        )
-                        .toList()),
-              ),
-              body: TabBarView(
-                  controller: _tabController,
-                  children: entityController.entity.display!.tabs!
-                      .map((e) => Container(
-                            child: e.type == "render" ? getRenderWidget(e.template!) : Container(),
-                          ))
-                      .toList())),
+        child: Column(
+          children: [
+            workflowTopTemp(context),
+            workflowBottomTemp(context, dropdownValue),
+          ],
         ),
       ),
+    );
+  }
+
+  Expanded workflowTopTemp(BuildContext context) {
+    return Expanded(
+      flex: 5,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2,
+        child: Scaffold(
+            backgroundColor: KC.background,
+            appBar: AppBar(
+              toolbarHeight: 80,
+              backgroundColor: KC.primary,
+              elevation: 1,
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.accessibility),
+                      SizedBox(width: 10),
+                      Text(
+                        "fullName",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 7),
+                  Row(
+                    children: [
+                      Text(
+                        "Tags : tags",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(width: 8),
+                      GestureDetector(
+                        onTap: () {
+                          // tagPopUp(context);
+                        },
+                        child: Icon(
+                          Icons.edit,
+                          size: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      homeController.reset();
+                    },
+                    icon: Icon(Icons.close))
+              ],
+              bottom: TabBar(
+                  controller: _tabController,
+                  tabs: entityController.entity.display!.tabs!
+                      .map(
+                        (e) => Tab(
+                          icon: Text(e.title.trTR),
+                        ),
+                      )
+                      .toList()),
+            ),
+            body: TabBarView(
+                controller: _tabController,
+                children: entityController.entity.display!.tabs!
+                    .map((e) => Container(
+                          child: e.type == "render"
+                              ? getRenderWidget(e.template!)
+                              : Container(),
+                        ))
+                    .toList())),
+      ),
+    );
+  }
+
+  Expanded workflowBottomTemp(BuildContext context, String dropdownValue) {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2,
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              workflowWidgets(dropdownValue, "State"),
+              workflowWidgets(dropdownValue, "Reset password progress"),
+              workflowWidgets(dropdownValue, "Available workflows"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row workflowWidgets(String dropdownValue, String title) {
+    return Row(
+      children: [
+        Text(
+          "$title : ",
+          textAlign: TextAlign.left,
+          style: TextStyle(color: Colors.black87, fontSize: 15),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        DropdownButton<String>(
+          value: dropdownValue,
+          icon: Icon(Icons.arrow_downward),
+          elevation: 1,
+          style: TextStyle(color: Colors.black87),
+          underline: Container(
+            height: 2,
+            color: KC.primary,
+          ),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        )
+      ],
     );
   }
 
