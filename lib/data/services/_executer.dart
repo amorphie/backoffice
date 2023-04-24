e1a223cfb3d1375474e4cf048774abbe283de8be
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2023-04-24 16:24:04 +0300

    executer

---
+++ lib/data/services/_executer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/foundation.dart';
+
+import 'common/executer_model.dart';
+
+class ExecuterManager {
+  static List<ExecuterModel> list = [];
+  static bool control(
+    String endpoint,
+    Map<String, dynamic>? data,
+    ResponseMethod method,
+  ) {
+    list.removeWhere((element) => element.isBeforeNow);
+    ExecuterModel model =
+        ExecuterModel(endpoint: endpoint, data: data, method: method);
+    if (list.contains(model)) {
+      print("responseTimeOverflow");
+      return false;
+    } else {
+      list.add(model);
+      return true;
+    }
+  }
+}
+
+class ExecuterModel {
+  String endpoint;
+  Map<String, dynamic>? data;
+  ResponseMethod method;
+  late DateTime time;
+  ExecuterModel({
+    required this.endpoint,
+    this.data,
+    required this.method,
+  }) {
+    this.time = DateTime.now();
+  }
+
+  bool get isBeforeNow =>
+      time.add(AppConfig.responseTime).isBefore(DateTime.now());
+
+  @override
+  bool operator ==(covariant ExecuterModel other) {
+    if (identical(this, other)) return true;
+
+    return other.endpoint == endpoint &&
+        mapEquals(other.data, data) &&
+        other.method == method;
+  }
+
+  @override
+  int get hashCode => endpoint.hashCode ^ data.hashCode ^ method.hashCode;
+
+  @override
+  String toString() {
+    return 'ExecuterModel(endpoint: $endpoint, data: $data, method: $method, time: $time)';
+  }
+}
