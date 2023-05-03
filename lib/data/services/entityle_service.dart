fdf92844b876c72797ccb09cb2506b550e710b47
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2023-05-03 16:53:45 +0300

    example entity service

--- lib/data/services/entityle_service.dart
+++ lib/data/services/entityle_service.dart
@@ -1,1 +1,16 @@
-class EntityService {}
+import 'common/response_model.dart';
+import 'executer_service.dart';
+
+class EntityService {
+  Future<ResponseModel> getResourceById(
+      {required String url,
+      required String pageSize,
+      required String pageNumber}) async {
+    ResponseModel response = await Executer.get(
+        endpoint: Uri(
+      path: url +
+          "?pageSize=$pageSize&page=$pageNumber ?? entity.search!.defaultPageNumber}",
+    ));
+    return response;
+  }
+}
