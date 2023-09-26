d2c6222d4dd1da2dfc93ea16825c2ab4ff825478
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2023-09-26 11:44:42 +0300

    resource role/rolegroup - empty list widget added

---
+++ lib/ui/widgets/empty_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../style/colors.dart';
+
+class EmptyList extends StatelessWidget {
+  const EmptyList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.list, size: 60, color: KC.primary),
+          SizedBox(height: 20),
+          Text("No items yet"),
+        ],
+      ),
+    );
+  }
+}
