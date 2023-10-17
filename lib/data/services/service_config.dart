789f857102f7b42fe10526b3d32b05f004fa43c8
selimcankacar <71877746+selimcankacar@users.noreply.github.com>
2023-10-17 15:54:49 +0300

    Update service_config.dart

--- lib/data/services/service_config.dart
+++ lib/data/services/service_config.dart
@@ -2,7 +2,7 @@ class ServiceConfig {
   static const Duration responseTime = Duration(seconds: 1);
   static const String apiVersion = "1.0";
 
-  static const String tagUrl = "tagUrl";
-  static const String baseUrl = "baseUrl";
-  static const String resourceUrl = "resourceUrl";
+  static const String tagUrl = "tagurl";
+  static const String baseUrl = "baseurl";
+  static const String resourceUrl = "resourceurl";
 }
